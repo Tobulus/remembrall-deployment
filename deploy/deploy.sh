2082e542e4839ee6a2f63b0c501256ae03d732ab
Tobsucht <tobiasbagg@googlemail.com>
2020-05-27 21:21:32 +0200

    Add functionality to fully deploy a container and restart the app

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-# deploys a tagged docker image to a given server
 
-# full server path, e.g. "root@192.168.0.0.1:/data"
-server_path=$1
-# the tag of the docker image which should e deployes
-docker_tag=$2
+# full server path, e.g. "root@192.168.0.0.1"
+server=$1
+# the docker image which should be deployed
+image=$2
 # the path where the docker image can be saved temporarily
-tmp_file="/tmp/${RANDOM}.tar.gz"
+tmp_file_name="/tmp/${RANDOM}.tar.gz"
 
 echo "Saving docker image to compressed file ..."
-docker save ${docker_tag} | gzip > ${tmp_file}
+docker save ${image} | gzip > ${tmp_file}
 echo "Created file '${tmp_file}'"
 
-# scp $tmp_file $server_path
+scp $tmp_file '${server}/tmp'
+echo "Copied file to server"
 
 rm $tmp_file
-echo "Deleted file '${tmp_file}'"
+echo "Deleted local file '${tmp_file_name}'"
+
+ssh $server "
+   docker load ${tmp_file_name}
+   rm ${tmp_file_name}
+   cd /data
+   docker-compose down
+   IMAGE=${image} docker-compose up
+"
+echo "Restarted application"
 
